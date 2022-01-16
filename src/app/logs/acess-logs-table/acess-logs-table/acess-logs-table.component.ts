4d05ed3050d88d54f5268f406f6b7a4c70907414
Leonardo Arruda <leonardo.earruda@gmail.com>
2022-01-16 20:52:51 -0300

    third commit

--- src/app/logs/acess-logs-table/acess-logs-table/acess-logs-table.component.ts
+++ src/app/logs/acess-logs-table/acess-logs-table/acess-logs-table.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Log } from 'src/app/modules/dto/log-dto';
 import { MatTableDataSource } from '@angular/material/table';
 import { LogService } from 'src/app/modules/services/logs/logs.service';
-import { FormControl } from '@angular/forms';
+import { FormArray, FormControl } from '@angular/forms';
+import { MetodoAberturaEnum, TipoDoEventoEnum } from 'src/app/modules/enums/dados.enum';
+import moment from 'moment';
 
 @Component({
   selector: 'app-acess-logs-table',
@@ -10,9 +12,30 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./acess-logs-table.component.scss'],
 })
 export class AcessLogsTableComponent implements OnInit {
-  filterInput = new FormControl('', { updateOn: 'blur' });
+  visualizadoPorUsuario = true;
+  allLogs: Log[];
+  filterInput = new FormControl('');
   dataSource = new MatTableDataSource<Log>();
-  displayedColumns = ['lockId', 'logId', 'estadoAtualFechadura', 'metodoAbertura', 'macAdress', 'userName'];
+  displayedColumns = [
+    'lockId',
+    'logId',
+    'estadoAtualFechadura',
+    'metodoAbertura',
+    'macAdress',
+    'userName',
+    'tipoEvento',
+    'horarioEvento',
+  ];
+  displayedColumnsUsuario = [
+    'userId',
+    'logId',
+    'estadoAtualFechadura',
+    'metodoAbertura',
+    'macAdress',
+    'userName',
+    'tipoEvento',
+    'horarioEvento',
+  ];
   constructor(private logService: LogService) {}
 
   ngOnInit(): void {
@@ -20,20 +43,42 @@ export class AcessLogsTableComponent implements OnInit {
   }
 
   public fetchLogs() {
-    this.dataSource.data = [];
     this.logService.fetchData().subscribe((res) => {
       res.reverse();
+      this.allLogs = res;
       this.dataSource.data = res;
     });
   }
 
-  public teste() {}
-
-  public visualizarPorUserId() {
-    this.displayedColumns.splice(0, 1, 'userId');
+  public filtro() {
+    this.dataSource.data = this.allLogs.filter((a) => {
+      if (this.visualizadoPorUsuario) {
+        const listraFiltrada =
+          a.payload.userName.includes(this.filterInput.value) || a.payload.userId.includes(this.filterInput.value);
+        return listraFiltrada;
+      }
+      return a.lockId.includes(this.filterInput.value) || a.mac.includes(this.filterInput.value);
+    });
   }
 
-  public visualizarPorLockId() {
-    this.displayedColumns.splice(0, 1, 'lockId');
+  public getTableText(text: number) {
+    return MetodoAberturaEnum[text];
+  }
+
+  public getEventTypeText(text: number) {
+    return TipoDoEventoEnum[text];
+  }
+
+  public epochToDate(epochDate: number) {
+    const humanDate = new Date(epochDate);
+    const humanDateFormatado = moment(humanDate).format('DD/MM/YYYY, HH:mm:ss');
+    return humanDateFormatado;
+  }
+
+  public getDisplayedColumns() {
+    if (this.visualizadoPorUsuario) {
+      return this.displayedColumnsUsuario;
+    }
+    return this.displayedColumns;
   }
 }
--- src/app/modules/enums/dados.enum.ts
+++ src/app/modules/enums/dados.enum.ts
@@ -1,5 +1,9 @@
-export enum SituacaoEnum {
-  APLICATIVO = "APLICATIVO",
-  USAR = "APLICATIVO"
+export enum MetodoAberturaEnum {
+  Digital = 3,
+  Aplicativo = 4,
 }
 
+export enum TipoDoEventoEnum {
+  Abertura = 1,
+  Trancamento = 2,
+}
